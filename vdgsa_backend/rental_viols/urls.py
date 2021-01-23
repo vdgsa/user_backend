8b4ea1c881a5e2d52a5bd6bf9f5290768f50947c
James Perretta <jameslp@umich.edu>
2021-01-23 12:31:15 -0500

    Added ListBowsView and skeleton BowDetailView.

--- vdgsa_backend/rental_viols/urls.py
+++ vdgsa_backend/rental_viols/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('bows/', views.ListBowsView.as_view(), name='list-bows'),
+    path('bows/<int:pk>/', views.BowDetailView.as_view(), name='bow-detail'),
     path('bows/add/', views.AddBowView.as_view(), name='add-bow'),
 ]
--- vdgsa_backend/rental_viols/views.py
+++ vdgsa_backend/rental_viols/views.py
@@ -1,10 +1,17 @@
-from vdgsa_backend.rental_viols.permissions import is_rental_manager
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.forms import ModelForm
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from vdgsa_backend.rental_viols.models import Bow
+from vdgsa_backend.rental_viols.permissions import is_rental_manager
+
+
+class RentalViewBase(LoginRequiredMixin, UserPassesTestMixin):
+    def test_func(self) -> bool:
+        return is_rental_manager(self.request.user)
 
 
 class BowForm(ModelForm):
@@ -22,11 +29,18 @@ class BowForm(ModelForm):
         ]
 
 
-class AddBowView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+class AddBowView(RentalViewBase, CreateView):
     model = Bow
     form_class = BowForm
 
-    template_name = 'add_bow.html'
+    template_name = 'bows/add_bow.html'
 
-    def test_func(self) -> bool:
-        return is_rental_manager(self.request.user)
+
+class ListBowsView(RentalViewBase, ListView):
+    model = Bow
+    template_name = 'bows/list_bows.html'
+
+
+class BowDetailView(RentalViewBase, DetailView):
+    model = Bow
+    template_name = 'bows/bow_detail.html'
