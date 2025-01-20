aa69afff2050252cad6f5f48e3dddd05f2cc8435
James Perretta <jameslp@umich.edu>
2025-01-20 10:12:58 -0500

    Add convenience script for starting local postgres server with docker

---
+++ dev_scripts/start_postgres.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+docker run -itd \
+    --name vdgsa-dev-postgres \
+    -v vdgsa-dev-postgres-data:/var/lib/postgresql/data/ \
+    -p 127.0.0.1:5432:5432 \
+    -e POSTGRES_DB=vdgsa_postgres \
+    -e POSTGRES_PASSWORD=postgres \
+    postgres:13
